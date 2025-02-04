b09001c2764f46025890544ebdad68bceb8c0ec1
braianCg <166315887+braianCg@users.noreply.github.com>
2025-02-04 20:07:14 -0300

    Update index.js

--- src/app/index.js
+++ src/app/index.js
@@ -11,58 +11,6 @@ const initApp = () => {
     app.use('/', express.static(config.dirname + 'public'))
     app.use(logger)
 
-    /**
-   * @typedef {Object} Gatito
-   * @property {string} name
-   * @property {string} raza
-   * @property {string} id
-   */
-
-    const gatitos = []
-
-    app.post('/api/gatito', (req, res) => {
-        /**
-         * @type {Gatito}
-         */
-        const gatito = req.body
-
-        if (!gatito.name || !gatito.raza) {
-            return res.status(400).send({
-                message: 'Error al crear un gatito',
-                error: 'Valores incompletos',
-            })
-        }
-        gatito.id = gatitos.length + 1
-
-        gatitos.push(gatito)
-
-        return res.status(200).send({
-            message: 'Gatito creada',
-            data: { ...gatito },
-        })
-
-        // res.status(304).redirect('/virtual')
-    })
-
-    const middlewareHome = (req, res, next) => {
-        req.mensajeBienvenida = 'Bienvenido al home gato!'
-        // res.send('ok')
-        next()
-    }
-
-    app.get('/api/home', middlewareHome, (req, res) => {
-        // throw new Error('Oops ocurrio un error')
-        res.status(200).send({ message: req.mensajeBienvenida })
-    })
-
-    app.use('/api/pets', PetsRouter)
-    app.use('/api/users', UsersRouter)
-
-    // app.use('/pets', PetsRouter)
-
-    // app.use(express.static('public'))
-
-    // app.use('/virtual', express.static('public'))
 
 app.use('/api/products', ProductsRouter)
 app.use('/api/cart', CartRouter)
